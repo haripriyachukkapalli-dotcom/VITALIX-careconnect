6c4ecf4487cfbd1f2db91899bc1fc84bd7a3d5fd
haripriyachukkapalli-dotcom <haripriyachukkapalli@gmail.com>
2026-09-20 12:43:48 +0530

    Remove Firebase setup and related imports
    Removed Firebase configuration and imports from script.js.

--- script.js
+++ script.js
@@ -1,47 +1,4 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
-
-import {
-    getAuth,
-    RecaptchaVerifier,
-    signInWithPhoneNumber
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-auth.js";
-
-import {
-    getFirestore,
-    doc,
-    setDoc
-} from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
-
-
-// ===============================
-// FIREBASE CONFIG
-// ===============================
-
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
-
-const app = initializeApp(firebaseConfig);
-
-const auth = getAuth(app);
-const db = getFirestore(app);
-
-
-// ===============================
-// OTP VARIABLES
-// ===============================
-
-let confirmationResult = null;
-
-
-// ===============================
-// SEND OTP
-// ===============================
+ic
 
 window.sendOTP = async function () {
 
